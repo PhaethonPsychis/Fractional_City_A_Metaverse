d9c69b25a63cd43f717bf9722367ef6085ee9f8d
Phaethon Psychis <80890815+PhaethonPsychis@users.noreply.github.com>
2022-04-19 12:32:38 +0300

    Update AutonomousMetaverseCities.sol

--- AutonomousMetaverseCities.sol
+++ AutonomousMetaverseCities.sol
@@ -6,15 +6,24 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 //import "@openzeppelin/contracts/utils/Context.sol";
 import "https://github.com/PhaethonPsychis/Fractional_City_A_Metaverse/blob/main/DDRLToken.sol";
 
-// a vending machine for selling votes at a fixed price
-// $DDRL is an Ethereum token that gives holders governance rights in Decentralised Design and Research Lab. 
-// The DDRL Token governs the development of the "Autonomous Cities in the Metaverse" project, giving holders the right to vote 
-// on the direction and implementation of the project. The more DDRL tokens a user has locked in their voting contract, 
-// the greater the decision-making power 
+
+// ***** Autonomous Metaverse Cities *****
+// AMECX is a model for the development of Private Cities backed by fractional ownership in the city's revenue streams
+// 
+// ***** Decentralized Design Research Lab DDRL **********
+// DDRL is a cyber-urban network where communities create new ways on how the design and production 
+// of architecture and urban environments is produced, and consumed and how citizens 
+// participate and learn. 
 //
-//  ***** Decentralized Design Research Lab DDRL **********
-// Autonomous Metaverse Cities is a solution and a model for the development of Private Cities backed by 
-//  fractional ownership in the city's revenue streams
+// ***** $DDRL Decentralized Community Governance Token *****
+// $DDRL is an Ethereum token that gives holders governance rights over the development, direction and implementation of the "Autonomous Metaverse Cities" project. 
+// The more DDRL tokens a user has locked in their voting contract, the greater the decision-making power
+//
+// ***** SellMeVotes *****
+// This is a vending machine that runs on blockchain selling votes at a fixed price
+//  
+  
+
 
 contract vendingMachineDDRL is Ownable{
 
